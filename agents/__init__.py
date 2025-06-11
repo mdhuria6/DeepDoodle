66bb9fc47dbbde50212376b45552a009fbc42b3a
Rishav Goswami <rishavg@iisc.ac.in>
2025-06-11 18:14:40 +0530

    renamed files for uniformity

--- agents/__init__.py
+++ agents/__init__.py
@@ -2,16 +2,16 @@ from .story_analyst import story_analyst
 from .scene_decomposer import scene_decomposer
 from .prompt_engineer import prompt_engineer
 from .image_generator import image_generator
-from .caption_agent import captioner  # Renamed from caption_agent
+from .captioner import captioner
 from .page_composer import page_composer
-from .panel_sizer_agent import panel_sizer  # Renamed from panel_sizer_agent
+from .panel_sizer import panel_sizer
 
 __all__ = [
     "story_analyst",
     "scene_decomposer",
     "prompt_engineer",
     "image_generator",
-    "panel_sizer",  # Renamed
-    "captioner",    # Renamed
+    "panel_sizer",
+    "captioner",
     "page_composer",
 ]
