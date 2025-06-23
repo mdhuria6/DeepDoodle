5698f0372f66fbc6d871737c0d1618800ef4de56
Kshitiz Singh <kshitiz.singh@walmart.com>
2025-06-24 03:58:40 +0530

    bug fix

--- test/sample_blip_captioner.py
+++
@@ -1,7 +0,0 @@
-from agnets.blip_captioner import blip_caption_folder
-
-# Caption all images in the output folder (change path as needed)
-captions = blip_caption_folder("output/panels")  # or "output/" if that's your image dir
-print(captions)
-
-# Now you can use `captions` for BERT score validation
